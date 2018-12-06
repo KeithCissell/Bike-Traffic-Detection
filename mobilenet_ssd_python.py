393c5586ad15e731d33d4159dde4df13a6ede53d
Keith A Cissell <keithacissell@gmail.com>
2018-12-06 00:14:53 -0600

    New test videos. RGB hist equalization for night scenes.

--- mobilenet_ssd_python.py
+++ mobilenet_ssd_python.py
@@ -5,11 +5,17 @@ import cv2
 from pprint import pprint
 from overlap import is_overlap
 
+
+# Variables
+video = 'night_rider_bike_film_short.mp4'
+equalize = True
+
+
 cycles_and_humans = {'bicycle':[], 'person':[]}
-# construct the argument parse 
+# construct the argument parse
 parser = argparse.ArgumentParser(
     description='Script to run MobileNet-SSD object detection network ')
-parser.add_argument("--video", default='motor_bike.mp4', help="path to video file. If empty, camera's stream will be used")
+parser.add_argument("--video", default=video, help="path to video file. If empty, camera's stream will be used")
 parser.add_argument("--prototxt", default="MobileNetSSD_deploy.prototxt",
                                   help='Path to text network file: '
                                        'MobileNetSSD_deploy.prototxt for Caffe model or '
@@ -29,59 +35,78 @@ classNames = { 0: 'background',
     14: 'motorbike', 15: 'person', 16: 'pottedplant',
     17: 'sheep', 18: 'sofa', 19: 'train', 20: 'tvmonitor' }
 
-# Open video file or capture device. 
+# Open video file or capture device.
 if args.video:
     cap = cv2.VideoCapture(args.video)
 else:
     cap = cv2.VideoCapture(0)
 
-#Load the Caffe model 
+#Load the Caffe model
 net = cv2.dnn.readNetFromCaffe(args.prototxt, args.weights)
 
 while True:
     # Capture frame-by-frame
     ret, frame = cap.read()
-    frame_resized = cv2.resize(frame,(300,300)) # resize frame for prediction
+
+
+    # Equalize histogram
+    if (equalize):
+        # img_yuv = cv2.cvtColor(frame, cv2.COLOR_BGR2YUV)
+        # img_yuv[:,:,0] = cv2.equalizeHist(img_yuv[:,:,0])
+        # frame = cv2.cvtColor(img_yuv, cv2.COLOR_YUV2BGR)
+
+        channels = cv2.split(frame)
+        eq_channels = []
+        for ch, color in zip(channels, ['B', 'G', 'R']):
+            eq_channels.append(cv2.equalizeHist(ch))
+
+        eq_image = cv2.merge(eq_channels)
+        frame = cv2.cvtColor(eq_image, cv2.COLOR_BGR2RGB)
+
+
+    # resize frame for prediction
+    frame_resized = cv2.resize(frame,(300,300))
+
 
     # MobileNet requires fixed dimensions for input image(s)
     # so we have to ensure that it is resized to 300x300 pixels.
-    # set a scale factor to image because network the objects has differents size. 
+    # set a scale factor to image because network the objects has differents size.
     # We perform a mean subtraction (127.5, 127.5, 127.5) to normalize the input;
     # after executing this command our "blob" now has the shape:
     # (1, 3, 300, 300)
     blob = cv2.dnn.blobFromImage(frame_resized, 0.007843, (300, 300), (127.5, 127.5, 127.5), False)
-    #Set to network the input blob 
+    #Set to network the input blob
     net.setInput(blob)
     #Prediction of network
     detections = net.forward()
 
     #Size of frame resize (300x300)
-    cols = frame_resized.shape[1] 
+    cols = frame_resized.shape[1]
     rows = frame_resized.shape[0]
 
-    #For get the class and location of object detected, 
+    #For get the class and location of object detected,
     # There is a fix index for class, location and confidence
     # value in @detections array .
     for i in range(detections.shape[2]):
-        confidence = detections[0, 0, i, 2] #Confidence of prediction 
-        if confidence > args.thr: # Filter prediction 
+        confidence = detections[0, 0, i, 2] #Confidence of prediction
+        if confidence > args.thr: # Filter prediction
             class_id = int(detections[0, 0, i, 1]) # Class label
 
-            # Object location 
-            xLeftBottom = int(detections[0, 0, i, 3] * cols) 
+            # Object location
+            xLeftBottom = int(detections[0, 0, i, 3] * cols)
             yLeftBottom = int(detections[0, 0, i, 4] * rows)
             xRightTop   = int(detections[0, 0, i, 5] * cols)
             yRightTop   = int(detections[0, 0, i, 6] * rows)
-            
+
             # Factor for scale to original size of frame
-            heightFactor = frame.shape[0]/300.0  
-            widthFactor = frame.shape[1]/300.0 
+            heightFactor = frame.shape[0]/300.0
+            widthFactor = frame.shape[1]/300.0
             # Scale object detection to frame
-            xLeftBottom = int(widthFactor * xLeftBottom) 
+            xLeftBottom = int(widthFactor * xLeftBottom)
             yLeftBottom = int(heightFactor * yLeftBottom)
             xRightTop   = int(widthFactor * xRightTop)
             yRightTop   = int(heightFactor * yRightTop)
-            # Draw location of object  
+            # Draw location of object
             cv2.rectangle(frame, (xLeftBottom, yLeftBottom), (xRightTop, yRightTop),
                           (0, 255, 0))
 
@@ -107,5 +132,5 @@ while True:
             is_overlap(bicycle, person)
     cv2.namedWindow("frame", cv2.WINDOW_NORMAL)
     cv2.imshow("frame", frame)
-    if cv2.waitKey(1) >= 0:  # Break with ESC 
+    if cv2.waitKey(1) >= 0:  # Break with ESC
         break
